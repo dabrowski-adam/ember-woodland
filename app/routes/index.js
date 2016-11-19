b397379f4d7469830f99a8238b1cc8d3e1ed7bb3
Adam Dąbrowski <contact@adamdabrowski.com>
2016-11-19 23:20:00 +0100

    Use services to store data.

---
+++ app/routes/index.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    model() {
+        const store = this.get("store");
+        return store.getProducts();
+    },
+    store: Ember.inject.service()
+});
--- app/services/store.js
+++ app/services/store.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
+import LineItem from 'woodland/models/line-item';
+import Order from 'woodland/models/order';
+import Product from 'woodland/models/product';
+
+const products = [
+    Product.create({title: "Tent", price: 10, description: "Cool item. ", imageUrl: ""}),
+    Product.create({title: "Sleeping Bag", price: 5, description: "Cool item. ", imageUrl: ""}),
+    Product.create({title: "Flashlight", price: 2, description: "Cool item. ", imageUrl: ""}),
+    Product.create({title: "First-Aid Kit", price: 3, description: "Cool item. ", imageUrl: ""})
+];
+
+const orders = [
+    Order.create({id: "1234", name: "Blaise Blobfish", items: [
+        LineItem.create({product: products[0], quantity: 1}), 
+        LineItem.create({product: products[1], quantity: 2}), 
+        LineItem.create({product: products[2], quantity: 1})
+    ]})
+];
 
 export default Ember.Service.extend({
     getOrderById(id) {
@@ -6,9 +24,9 @@ export default Ember.Service.extend({
         return orders.findBy("id", id);
     },
     getOrders() {
-        return [
-            {id: "1", name:"Nate"},
-            {id: "2", name:"Gregg"}
-        ];
+        return orders;
+    },
+    getProducts() {
+        return products;
     }
 });
